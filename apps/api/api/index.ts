e38e9a2b6815ffa895d82c041ef7027608316b0b
Matt Hova <matthova87@gmail.com>
2026-03-25 22:40:58 -0700

    ok

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -1,5 +1,5 @@
 import { handle } from "@hono/node-server/vercel";
-import app from "../src/app";
-import "../src/env";
+import app from "../dist/src/app.js";
+import "../dist/src/env.js";
 
 export default handle(app);
